439036178b0f9b0fcd7382763c36818f347e14a6
dvforest <dvforest@hotmail.com>
2026-02-13 22:43:30 -0500

    Implement prepend method and fix value assignment with Node class

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,6 +1,6 @@
 class Node {
-    constructor() {
-        this.value = null;
+    constructor(value) {
+        this.value = value;
         this.next = null;
     }
 }
@@ -14,14 +14,31 @@ class LinkedList {
     appendValue(value) {
         const newNode = new Node(value);
 
-        // If empty list, add node as head
+        // If empty list, add node as head and tail
         if (!this.head) {
-            this.head === new Node();
+            this.head = newNode;
+            this.tail = newNode;
             return;
         }
 
         // Else, assign as new tail
         this.tail.next = newNode;
         this.tail = newNode;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+
+        // If list not empty, shift it to next position
+        if (this.head) {
+            newNode.next = this.head;
+        }
+
+        this.head = newNode;
+
+        // If list was empty, update tail
+        if (!this.tail) {
+            this.tail = newNode;
+        }
     }
 }
